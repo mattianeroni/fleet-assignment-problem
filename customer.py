bee095c419daf6291f3be57af5eccdc1111ca40b
mattia.neroni <mattia.neroni@unipr.it>
2022-04-11 11:52:08 +0200

    problem reading

--- customer.py
+++
@@ -1,6 +0,0 @@
-class Customer:
-    def __init__(self, id, demand):
-        self.id = id
-        self.demand = demand
-        self.assigned = False
-        self.assigned_qty = 0
---
+++ src/main.py
@@ -0,0 +1,29 @@
+"""
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+This project concerns the development of a set of algorithms for solving the fleet 
+assignment problem under additional constraints:
+    - Limited capacity of fleets
+    - Minimum load for fleets
+    - Green capacity of fleets
+    - Stochastic demand and stochastic productivity
+    
+This problem is completely new in scientific literature and the proposed solutions 
+are supporteed and validated by a case study based on real data. 
+
+
+Author: Mattia Neroni, Ph.D., Eng.
+Contact: mneroni@uoc.edu
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+"""
+import utils 
+
+
+
+if __name__ == "__main__":
+
+    problem = read_problem()
+
+
+
+
+    print("Well done \U0001F49A !")
